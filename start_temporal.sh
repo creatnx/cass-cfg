3c1874f55462e30064054ab95f44e35277338f9c
dai.pham <daihoangpham@gmail.com>
2022-06-07 20:08:47 +0700

    Update start_temporal.sh

--- start_temporal.sh
+++ start_temporal.sh
@@ -1,5 +1,8 @@
 node_host=$(hostname -I | awk '{print $1}') envsubst < ./docker.yaml > config.yaml
 sudo docker run \
     --network=host \
-    --name ${service} \
+    --name frontend \
+    -e env=docker \
+    -v /home/hoangdai.pham/cass-cfg/dynamic.yaml:/tmp/temporal/dynamic_config.yaml \
+    -v /home/hoangdai.pham/cass-cfg/config.yaml:/etc/temporal/config/docker.yaml \
     temporalio/server:latest
