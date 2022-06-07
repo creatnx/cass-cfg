f20c61abd39645245da5f673cfaa1788a67e9223
dai.pham <daihoangpham@gmail.com>
2022-06-07 20:20:56 +0700

    Update start_history.sh

--- start_history.sh
+++ start_history.sh
@@ -1,3 +1,3 @@
 node_host=$(hostname -I | awk '{print $1}') envsubst < ./docker.yaml > ./config.yaml
 touch out.log
-./server --env=config --config=.. start --service=history > out.log &
+./server --env=config --config=. start --service=history > out.log &
