023b72bac097ed8bbb8a45b6e28a220d09430df0
dai.pham <daihoangpham@gmail.com>
2022-06-07 20:15:24 +0700

    Update start_history.sh

--- start_history.sh
+++ start_history.sh
@@ -1,2 +1,3 @@
 node_host=$(hostname -I | awk '{print $1}') envsubst < ./cass-cfg/docker.yaml > ./cass-cfg/config.yaml
-./temporal-server --env=config --config=./cass-cfg start --service=history
+touch out.log
+./temporal-server --env=config --config=./cass-cfg start --service=history > out.log &
